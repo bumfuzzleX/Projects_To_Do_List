de1ca0039f1e92247a1d7f8d62352701ff9544ab
Veerhan-glitch <praveersinghchauhan@yahoo.com>
2021-09-05 20:50:20 +0530

    Revert "Delete Projects_To_Do_List.py"
    This reverts commit a28b03c3d8c8d4677cf44cc68ff57786e5e16dd8.

---
+++ Projects_To_Do_List.py
@@ -0,0 +1,59 @@
+def Projects_To_Do_List():
+    Running = True
+    while Running:
+        import pickle
+        Projects = pickle.load(open("Projects_To_Do_List/Projects.dat","rb"))
+        print("Welcome to projects to do list!")
+        while True:
+            choice = input("Enter 'a' for adding a project, 'r' for removing a project, 'v' for vewing all the stored projects and 'e' for exiting: ").lower()
+            if choice == 'a':
+                print(f"The projects present are: {Projects}")
+                while True:
+                    amount = input("How many Projects do you want to add? ")
+                    if amount.isdigit():
+                        amount = int(amount)
+                        break
+                    else:
+                        print("Invalid input! Please try again!")
+                for i in range(amount):
+                    add = input("Enter the name of the project you want to add: ")
+                    Projects.append(add)
+                print(f"Now the projects present are: {Projects}")
+                break
+            elif choice == 'r':
+                print(f"The avalable projects are: {Projects}")
+                while True:
+                    amount = input("How many Projects do you want to remove? ")
+                    if amount.isdigit():
+                        amount = int(amount)
+                        break
+                    else:
+                        print("Invalid input! Please try again!")
+                for i in range(amount):
+                    remove = input("Enter the name of the project you want to remove: ")
+                    Projects.remove(remove)
+                print(f"Now the projects present are: {Projects}")
+                break
+            elif choice == 'v':
+                print(f"The projects are: {Projects}")
+                break
+            elif choice == 'e':
+                print("Thank you for using!.")
+                pickle.dump(Projects,open("Projects_To_Do_List/Projects.dat","wb"))
+                break
+            else:
+                print("Invalid input! Please try again!")
+        if choice == 'e':
+            break
+        pickle.dump(Projects,open("Projects_To_Do_List/Projects.dat","wb"))
+        while True:
+            Again=input("Do you want to edit again? (y/n)").lower()
+            if Again == 'y':
+                break
+            elif Again == 'n':
+                Running = False
+                print("Thank you for using!.")
+                break
+            else:
+                print("Invalid input! Please try again!")
+Projects_To_Do_List()
